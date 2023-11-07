0d53f3d60c599c40729eecc200bceaa358ea2d02
AsadAliKhan21 <138221599+AsadAliKhan21@users.noreply.github.com>
2023-11-07 05:53:32 +0500

    Add scores in html & refactor js code

--- script.js
+++ script.js
@@ -1,57 +1,114 @@
-const choices = ["Rock", "Paper", "Scissors"];
+"use strict";
 
-const btnRock = document.querySelector('.opt-rock');
-const btnPaper = document.querySelector('.opt-paper');
-const btnScissors = document.querySelector('opt-scissors');
+const btnRock = document.querySelector(".opt-rock");
+const btnPaper = document.querySelector(".opt-paper");
+const btnScissors = document.querySelector(".opt-scissors");
 
-btnRock.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-})
+let playerScore = 0;
+let computerScore = 0;
 
-btnPaper.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-})
+btnRock.addEventListener("click", () => {
+    playRound("ROCK", getComputerChoice());
+});
 
-btnScissors.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-})
+btnPaper.addEventListener("click", () => {
+    playRound("PAPER", getComputerChoice());
+});
 
-
+btnScissors.addEventListener("click", () => {
+    playRound("SCISSORS", getComputerChoice());
+});
 
 function getComputerChoice() {
+    const choices = ["ROCK", "PAPER", "SCISSORS"];
     const randIndex = Math.floor(Math.random() * choices.length);
     return choices[randIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-    const lowerCasePS = playerSelection.toLowerCase();
-    const lowerCaseCS = computerSelection.toLowerCase();
-    if (lowerCasePS === lowerCaseCS) {
-        return "It's a Tie!";
-    } else {
-        const win = "You Win! "
-        const lose = "You Lose! "
-        const paperOnRock = "Paper beats Rock";
-        const scissorOnPaper = "Scissors beat Paper";
-        const rockOnScissor = "Rock beat Scissors";
-        if (lowerCasePS === 'rock' && lowerCaseCS === 'paper') {
-            return lose + paperOnRock;
-        } else if (lowerCasePS ==='paper' && lowerCaseCS === 'rock') {
-            return win + paperOnRock;
-        } 
-        
-        else if (lowerCasePS === 'paper' && lowerCaseCS === 'scissors') {
-            return lose + scissorOnPaper;
-        } else if (lowerCasePS === 'scissors' && lowerCaseCS === 'paper') {
-            return win + scissorOnPaper;
-        } 
-        
-        else if (lowerCasePS === "scissors" && lowerCaseCS === 'rock') {
-            return lose + rockOnScissor;
-        } else {
-            return win + rockOnScissor;
-        }
+    const roundWinner = document.querySelector(".round-winner");
+
+    if (playerSelection === computerSelection) {
+        roundWinner.textContent = "It's a Tie!";
+        updateRoundInfo("");
+        updateCurrentOpt(playerSelection, computerSelection);
+        return;
     }
+
+    const win = "You Win!";
+    const lose = "You Lose!";
+    const paperOnRock = "Paper beats Rock";
+    const scissorsOnPaper = "Scissors beat Paper";
+    const rockOnScissor = "Rock beat Scissors";
+
+    updateCurrentOpt(playerSelection, computerSelection);
+
+    switch (playerSelection + "-" + computerSelection) {
+        case "ROCK-PAPER":
+            roundWinner.textContent = lose;
+            updateRoundInfo(paperOnRock);
+            break;
+        case "PAPER-ROCK":
+            roundWinner.textContent = win;
+            updateRoundInfo(paperOnRock);
+            break;
+        case "PAPER-SCISSORS":
+            roundWinner.textContent = lose;
+            updateRoundInfo(scissorsOnPaper);
+            break;
+        case "SCISSORS-PAPER":
+            roundWinner.textContent = win;
+            updateRoundInfo(scissorsOnPaper);
+            break;
+        case "SCISSORS-ROCK":
+            roundWinner.textContent = lose;
+            updateRoundInfo(rockOnScissor);
+            break;
+        case "ROCK-SCISSORS":
+            roundWinner.textContent = win;
+            updateRoundInfo(rockOnScissor);
+            break;
+        default:
+            break;
+    }
+
+    updateScore(roundWinner);
+}
+
+function updateScore(roundWinner) {
+    roundWinner = roundWinner.textContent;
+    if (roundWinner.includes("Win")) {
+        const plScoreEl = document.querySelector("#plScore");
+        playerScore++;
+        plScoreEl.textContent = playerScore;
+    } else if (roundWinner.includes("Lose")) {
+        const compScoreEl = document.querySelector("#compScore");
+        computerScore++;
+        compScoreEl.textContent = computerScore;
+    }
+}
+
+function updateRoundInfo(infoText) {
+    const roundInfo = document.querySelector(".round-info");
+    roundInfo.textContent = infoText;
+}
+
+function updateCurrentOpt(playerSelection, computerSelection) {
+    const plCurrentOpt = document.querySelector(".pl-current-opt");
+    const compCurrentOpt = document.querySelector(".comp-current-opt");
+
+    plCurrentOpt.textContent =
+        playerSelection === "ROCK"
+            ? "✊"
+            : playerSelection === "PAPER"
+            ? "✋"
+            : "✌";
+    compCurrentOpt.textContent =
+        computerSelection === "ROCK"
+            ? "✊"
+            : computerSelection === "PAPER"
+            ? "✋"
+            : "✌";
 }
 
 // function game() {
